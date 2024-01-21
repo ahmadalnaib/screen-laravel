fc9a9699de9b72f8277aff99561eeed807bfb72e
ahmedhabeeb73 <alnaib888@gmail.com>
2024-01-21 20:22:17 +0100

    add layouts

---
+++ resources/js/Pages/Videos/Create.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Capture" />
+
+    <AuthenticatedLayout>
+  
+
+        <div class="py-12">
+            <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">Video Capture</div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
